49db3fcffe38f91a803bbdb879420d45e2340749
Hudson <161895030+HudsonCarneiro@users.noreply.github.com>
2025-02-13 22:31:01 -0300

    script

--- JAVA SCRIPT/midnight-asphalt/Front-End/src/game.js
+++ JAVA SCRIPT/midnight-asphalt/Front-End/src/game.js
@@ -6,7 +6,7 @@ class MidnightAsphalt extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('player', '/assets/car_player.png');
+        this.load.image('player', './assets/car_player.png');
         this.load.image('road', '/assets/layout00.png');
         this.load.image('car2', '/assets/car002.png');
         this.load.image('car3', '/assets/car003.png');
@@ -20,5 +20,34 @@ class MidnightAsphalt extends Phaser.Scene {
         this.cursors = this.input.keyboard.createCursorKeys();
 
         this.car2 = this.physics.add.sprite(9275, -100, 'car2');
+        this.car3 = this.physics.add.sprite(1005, -300, 'car3');
+
+    }
+
+    update(){
+        this.road1.y += 5;
+        this.road2.y += 5;
+        if(this.road1.y >= 720) this.road1.y = -720;
+        if(this.road2.y >= 720) this.road2.y = -720;
+
+        if(this.cursors.left.isDown){
+            this.player.x -= 5;
+        }
+
+        this.car2.y += 5;
+        this.car3.y +=5;
+        if(this.car2.y > 800) this.car2.y = -100;
+        if(this.car3.y >800) this.car3.y = -300;
     }
 }
+const config = {
+    type: Phaser.AUTO,
+    width:1280,
+    height:720,
+    physics: {
+        default: 'arcade',
+        arcade:{ debug: false}
+    },
+    scene: MidnightAsphalt
+};
+const game = new Phaser.Game(config);
