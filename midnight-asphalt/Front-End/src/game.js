a2d479f974dd06e8787eb36cda6c0b6ce28511a6
Hudson <161895030+HudsonCarneiro@users.noreply.github.com>
2025-02-14 21:51:47 -0300

    js

--- midnight-asphalt/Front-End/src/game.js
+++ midnight-asphalt/Front-End/src/game.js
@@ -1,25 +1,19 @@
 import Phaser from 'phaser';
 
 class MidnightAsphalt extends Phaser.Scene {
-    private road1!: Phaser.GameObjects.TileSprite;
-    private road2!: Phaser.GameObjects.TileSprite;
-    private player!: Phaser.Physics.Arcade.Sprite;
-    private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
-    private car2!: Phaser.Physics.Arcade.Sprite;
-    private car3!: Phaser.Physics.Arcade.Sprite;
-
     constructor() {
         super("MidnightAsphalt");
     }
 
-    preload(): void {
+    preload() {
         this.load.image('player', './assets/car_player.png');
-        this.load.image('road', '/assets/layout00.png');
-        this.load.image('car2', '/assets/car002.png');
-        this.load.image('car3', '/assets/car003.png');
+        this.load.image('road', './assets/layout00.png');
+        this.load.image('car2', './assets/car002.png');
+        this.load.image('car3', './assets/car003.png');
+    
     }
 
-    create(): void {
+    create() {
         this.road1 = this.add.tileSprite(0, 0, 1280, 720, 'road').setOrigin(0, 0);
         this.road2 = this.add.tileSprite(0, -720, 1280, 720, 'road').setOrigin(0, 0);
 
@@ -30,7 +24,7 @@ class MidnightAsphalt extends Phaser.Scene {
         this.car3 = this.physics.add.sprite(1005, -300, 'car3');
     }
 
-    update(): void {
+    update() {
         this.road1.y += 5;
         this.road2.y += 5;
         if (this.road1.y >= 720) this.road1.y = -720;
@@ -38,6 +32,9 @@ class MidnightAsphalt extends Phaser.Scene {
 
         if (this.cursors.left.isDown) {
             this.player.x -= 5;
+        }
+        if (this.cursors.right.isDown) {
+            this.player.x += 5;
         }
 
         this.car2.y += 5;
@@ -47,7 +44,7 @@ class MidnightAsphalt extends Phaser.Scene {
     }
 }
 
-const config: Phaser.Types.Core.GameConfig = {
+const config = {
     type: Phaser.AUTO,
     width: 1280,
     height: 720,
